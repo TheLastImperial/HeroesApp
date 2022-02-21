5ca80590d6ec37fb92dcab98b075982aababff45
TheLastImperial <17633123+TheLastImperial@users.noreply.github.com>
2022-02-20 22:04:13 -0700

    Added Hero List.

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { HeroList } from '../hero/HeroList'
 
 export const DcScreen = () => {
   return (
-    <div>DcScreen</div>
+    <>
+      <h1>DcScreen</h1>
+      <HeroList publisher="DC Comics"/>
+    </>
   )
 }
--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { HeroList } from '../hero/HeroList'
 
 export const MarvelScreen = () => {
   return (
-    <div>MarvelScreen</div>
+    <>
+      <h1>MarvelScreen</h1>
+      <HeroList publisher="Marvel Comics"/>
+    </>
   )
 }
